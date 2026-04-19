932fc2d27af1316bf6e71877bedbd5f07e5cbc25
Arthur <arthurfelipe5567@gmail.com>
2026-04-19 00:05:49 -0300

    fix: add main and auth nav graphs
    also change states to adapt to new
    auth viewmodel

--- app/src/main/java/com/domus/homefy/MainActivity.kt
+++ app/src/main/java/com/domus/homefy/MainActivity.kt
@@ -8,13 +8,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.domus.homefy.data.AuthState
 import com.domus.homefy.ui.home.HomeScreen
-import com.domus.homefy.ui.home.LoginViewModel
-import com.domus.homefy.ui.login.LoginScreen
+import com.domus.homefy.ui.auth.AuthViewModel
+import com.domus.homefy.ui.auth.login.LoginScreen
 import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
@@ -31,48 +33,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AppNavigation() {
-    val navController = rememberNavController()
-    val loginViewModel: LoginViewModel = koinViewModel()
-    val isLogged by loginViewModel.isLogged.collectAsState()
+    val authViewModel: AuthViewModel = koinViewModel()
+    val authState by authViewModel.authState.collectAsState()
 
-    LaunchedEffect(isLogged) {
-        if (isLogged) {
-            navController.navigate("home") {
-                popUpTo("login") {
-                    inclusive = true
-                }
-            }
-        } else {
-            navController.navigate("login") {
-                popUpTo("login")
-            }
-
-        }
+    when (authState) {
+        AuthState.Authenticated -> MainNavGraph()
+        AuthState.NotAuthenticated -> AuthNavGraph()
+        else -> {}
+//        AuthState.NotAuthenticated -> LoginScreen()
     }
 
-    NavHost(navController = navController, startDestination = "home") {
-        composable("login") {
-            LoginScreen(loginViewModel)
-        }
+}
 
-        composable("home") {
-            HomeScreen(loginViewModel)
+@Composable
+fun AuthNavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            LoginScreen()
         }
     }
 }
 
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    HomefyTheme {
-//        Greeting("Android")
-//    }
-//}
+@Composable
+fun MainNavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen()
+        }
+    }
+
+}
