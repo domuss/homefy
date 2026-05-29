a47c402fdeca82cd207ff791b510fa576b4fdb98
Arthur <arthurfelipe5567@gmail.com>
2026-05-28 21:10:43 -0300

    fix: move isHouseAdmin to HouseRepository

--- app/src/main/java/com/domus/homefy/data/HouseRepository.kt
+++ app/src/main/java/com/domus/homefy/data/HouseRepository.kt
@@ -1,16 +1,18 @@
 package com.domus.homefy.data
 
 import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.postgrest.from
 import io.github.jan.supabase.postgrest.postgrest
+import io.github.jan.supabase.postgrest.query.Columns
 
 class HouseRepository(private val supabase: SupabaseClient) {
 
-    suspend fun  CriarCasa(house: House): Result<Unit>{
-            return try {
-                supabase.postgrest["home"].insert(house)
-                Result.success(Unit)
-            } catch (e: Exception){
-                Result.failure(e)
+    suspend fun CriarCasa(house: House): Result<Unit> {
+        return try {
+            supabase.postgrest["home"].insert(house)
+            Result.success(Unit)
+        } catch (e: Exception) {
+            Result.failure(e)
         }
     }
 
@@ -80,7 +82,6 @@ class HouseRepository(private val supabase: SupabaseClient) {
     }
 
 
-
     suspend fun getHouseByAccessCode(code: String): Result<House?> {
         return try {
             val house = supabase.postgrest["home"].select {
@@ -95,7 +96,11 @@ class HouseRepository(private val supabase: SupabaseClient) {
     }
 
 
-    suspend fun insertMember(houseId: Long, userId: Int, roleId: Int = Role.RESIDENT.id): Result<Unit> {
+    suspend fun insertMember(
+        houseId: Long,
+        userId: Int,
+        roleId: Int = Role.RESIDENT.id
+    ): Result<Unit> {
         return try {
             val member = HouseMember(house_id = houseId, user_id = userId, role_id = roleId)
             supabase.postgrest["house_members"].insert(member)
@@ -133,6 +138,60 @@ class HouseRepository(private val supabase: SupabaseClient) {
             Result.success(houses)
         } catch (e: Exception) {
             Result.failure(e)
+        }
+    }
+
+    suspend fun getHouseMembers(houseId: Long): Result<List<HouseMemberFull>> {
+        val columns = Columns.raw(
+            """
+            id,
+            supa_id,
+            name,
+            username,
+            house_members!inner(
+                id,
+                role_id
+            )
+        """.trimIndent()
+        )
+
+        return try {
+            val membersRaw = supabase.from("users").select(
+                columns = columns
+            ) {
+                filter {
+                    eq("house_members.house_id", houseId)
+                }
+            }.decodeList<HouseMemberSupabase>()
+
+            val members = mutableListOf<HouseMemberFull>()
+            membersRaw.forEach {
+                members.add(it.toModel())
+            }
+
+            Result.success(members)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    suspend fun isHouseAdmin(houseId: Long, userId: Long): Boolean {
+        val columns = Columns.raw(
+            """
+            id,
+        """.trimIndent()
+        )
+
+        return try {
+            supabase.from("house_members").select(columns = columns) {
+                filter {
+                    eq("house_id", houseId)
+                    eq("user_id", userId)
+                    eq("role_id", 1)
+                }
+            }.decodeSingleOrNull<Long>() != null
+        } catch (e: Exception) {
+            false
         }
     }
 }
